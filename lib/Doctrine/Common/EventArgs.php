07f8588bc199ded43c0d5b3670d88d09907659af
romanb <romanb@625475ce-881a-0410-a577-b389adb331d8>
2009-02-07 17:02:13 +0000

    Small reorganizations, improvements and progress.

---
+++ lib/Doctrine/Common/EventArgs.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ *  $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Doctrine\Common;
+
+/**
+ * EventArgs is the base class for classes containing event data.
+ *
+ * This class contains no event data and cannot be instantiated.
+ * It is used by events that do not pass state information to an event handler
+ * when an event is raised. The single empty EventArgs instance can be obtained
+ * through {@link getEmptyInstance()}.
+ *
+ * @author      Konsta Vesterinen <kvesteri@cc.hut.fi>
+ * @author      Roman Borschel
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @link        www.doctrine-project.org
+ * @since       2.0
+ * @version     $Revision$
+ */
+class EventArgs
+{
+    private static $_emptyEventArgsInstance;
+    private $_defaultPrevented;
+
+    protected function __construct()
+    {
+        $this->_defaultPrevented = false;
+    }
+
+    public function preventDefault()
+    {
+        $this->_defaultPrevented = true;
+    }
+
+    public function getDefaultPrevented()
+    {
+        return $this->_defaultPrevented;
+    }
+
+    public static function getEmptyInstance()
+    {
+        if ( ! self::$_emptyEventArgsInstance) {
+            self::$_emptyEventArgsInstance = new EventArgs;
+        }
+        return self::$_emptyEventArgsInstance;
+    }
+}  
