747a4e00ff878c93676e6f196d1322dfa04790bf
Rafael Dohms <rdohms@gmail.com>
2012-04-15 22:57:24 +0200

    Reviewed PHP DocBlocks
    Fixed missing docblocks
    Fixed a few spelling mistakes
    Fixed bad class references
    Fixed wrong variables names
    Fixed bad use of inheritdocs

--- lib/Doctrine/Common/EventManager.php
+++ lib/Doctrine/Common/EventManager.php
@@ -127,7 +127,7 @@ class EventManager
      * Adds an EventSubscriber. The subscriber is asked for all the events he is
      * interested in and added as a listener for these events.
      *
-     * @param Doctrine\Common\EventSubscriber $subscriber The subscriber.
+     * @param \Doctrine\Common\EventSubscriber $subscriber The subscriber.
      */
     public function addEventSubscriber(EventSubscriber $subscriber)
     {
