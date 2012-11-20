e0fe5e5e8818707231c93bc2fab627da23470d33
Ilya Sabelnikov <fruit.dev@gmail.com>
2012-11-21 01:27:02 +0200

    Replace file_exists() calls with is_file() where it is needed

--- tests/Doctrine/Tests/TestInit.php
+++ tests/Doctrine/Tests/TestInit.php
@@ -11,14 +11,14 @@ spl_autoload_register(function($class)
 {
     if (0 === strpos($class, 'Doctrine\Tests\\')) {
         $path = __DIR__.'/../../'.strtr($class, '\\', '/').'.php';
-        if (file_exists($path) && is_readable($path)) {
+        if (is_file($path) && is_readable($path)) {
             require_once $path;
 
             return true;
         }
     } else if (0 === strpos($class, 'Doctrine\Common\\')) {
         $path = __DIR__.'/../../../lib/'.($class = strtr($class, '\\', '/')).'.php';
-        if (file_exists($path) && is_readable($path)) {
+        if (is_file($path) && is_readable($path)) {
             require_once $path;
 
             return true;
