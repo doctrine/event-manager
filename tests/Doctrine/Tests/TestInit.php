fa2aa006c993574946078643994d2dcd0a5488ac
Benjamin Eberlei <kontakt@beberlei.de>
2013-01-10 23:12:36 +0100

    Use doctrine/inflector and deprecate the Common Util inflector.

--- tests/Doctrine/Tests/TestInit.php
+++ tests/Doctrine/Tests/TestInit.php
@@ -16,15 +16,10 @@ spl_autoload_register(function($class)
 
             return true;
         }
-    } else if (0 === strpos($class, 'Doctrine\Common\\')) {
-        $path = __DIR__.'/../../../lib/'.($class = strtr($class, '\\', '/')).'.php';
-        if (is_file($path) && is_readable($path)) {
-            require_once $path;
-
-            return true;
-        }
     }
 });
+
+require_once __DIR__ . "/../../../vendor/autoload.php";
 
 \Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace(
     'Doctrine\Tests\Common\Annotations\Fixtures', __DIR__ . '/../../'
