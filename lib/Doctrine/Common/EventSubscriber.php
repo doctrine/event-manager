5bc00dccf8844944fa20a40c4f71f2009b91cd5f
Michael Moravec <me@majkl.me>
2018-06-07 15:00:37 +0200

    Apply Doctrine CS 4.0

--- lib/Doctrine/Common/EventSubscriber.php
+++ lib/Doctrine/Common/EventSubscriber.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Doctrine\Common;
 
 /**
@@ -6,19 +9,13 @@ namespace Doctrine\Common;
  * If an EventSubscriber is added to an EventManager, the manager invokes
  * {@link getSubscribedEvents} and registers the subscriber as a listener for all
  * returned events.
- *
- * @link   www.doctrine-project.org
- * @since  2.0
- * @author Guilherme Blanco <guilhermeblanco@hotmail.com>
- * @author Jonathan Wage <jonwage@gmail.com>
- * @author Roman Borschel <roman@code-factory.org>
  */
 interface EventSubscriber
 {
     /**
      * Returns an array of events this subscriber wants to listen to.
      *
-     * @return array
+     * @return string[]
      */
     public function getSubscribedEvents();
 }
--- tests/Doctrine/Tests/Common/EventManagerTest.php
+++ tests/Doctrine/Tests/Common/EventManagerTest.php
@@ -2,36 +2,43 @@
 
 namespace Doctrine\Tests\Common;
 
-use Doctrine\Common\EventManager;
 use Doctrine\Common\EventArgs;
+use Doctrine\Common\EventManager;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\Tests\DoctrineTestCase;
+use function count;
 
-class EventManagerTest extends \Doctrine\Tests\DoctrineTestCase
+class EventManagerTest extends DoctrineTestCase
 {
     /* Some pseudo events */
     private const PRE_FOO  = 'preFoo';
     private const POST_FOO = 'postFoo';
     private const PRE_BAR  = 'preBar';
 
-    private $_preFooInvoked  = false;
+    /** @var bool */
+    private $_preFooInvoked = false;
+
+    /** @var bool */
     private $_postFooInvoked = false;
 
+    /** @var EventManager */
     private $_eventManager;
 
-    protected function setUp()
+    protected function setUp() : void
     {
-        $this->_eventManager   = new EventManager;
+        $this->_eventManager   = new EventManager();
         $this->_preFooInvoked  = false;
         $this->_postFooInvoked = false;
     }
 
-    public function testInitialState()
+    public function testInitialState() : void
     {
         self::assertEquals([], $this->_eventManager->getListeners());
         self::assertFalse($this->_eventManager->hasListeners(self::PRE_FOO));
         self::assertFalse($this->_eventManager->hasListeners(self::POST_FOO));
     }
 
-    public function testAddEventListener()
+    public function testAddEventListener() : void
     {
         $this->_eventManager->addEventListener(['preFoo', 'postFoo'], $this);
         self::assertTrue($this->_eventManager->hasListeners(self::PRE_FOO));
@@ -41,7 +48,7 @@ class EventManagerTest extends \Doctrine\Tests\DoctrineTestCase
         self::assertEquals(2, count($this->_eventManager->getListeners()));
     }
 
-    public function testDispatchEvent()
+    public function testDispatchEvent() : void
     {
         $this->_eventManager->addEventListener(['preFoo', 'postFoo'], $this);
         $this->_eventManager->dispatchEvent(self::PRE_FOO);
@@ -49,7 +56,7 @@ class EventManagerTest extends \Doctrine\Tests\DoctrineTestCase
         self::assertFalse($this->_postFooInvoked);
     }
 
-    public function testRemoveEventListener()
+    public function testRemoveEventListener() : void
     {
         $this->_eventManager->addEventListener(['preBar'], $this);
         self::assertTrue($this->_eventManager->hasListeners(self::PRE_BAR));
@@ -57,7 +64,7 @@ class EventManagerTest extends \Doctrine\Tests\DoctrineTestCase
         self::assertFalse($this->_eventManager->hasListeners(self::PRE_BAR));
     }
 
-    public function testAddEventSubscriber()
+    public function testAddEventSubscriber() : void
     {
         $eventSubscriber = new TestEventSubscriber();
         $this->_eventManager->addEventSubscriber($eventSubscriber);
@@ -67,20 +74,23 @@ class EventManagerTest extends \Doctrine\Tests\DoctrineTestCase
 
     /* Listener methods */
 
-    public function preFoo(EventArgs $e)
+    public function preFoo(EventArgs $e) : void
     {
         $this->_preFooInvoked = true;
     }
 
-    public function postFoo(EventArgs $e)
+    public function postFoo(EventArgs $e) : void
     {
         $this->_postFooInvoked = true;
     }
 }
 
-class TestEventSubscriber implements \Doctrine\Common\EventSubscriber
+class TestEventSubscriber implements EventSubscriber
 {
-    public function getSubscribedEvents()
+    /**
+     * @return string[]
+     */
+    public function getSubscribedEvents() : array
     {
         return ['preFoo', 'postFoo'];
     }
