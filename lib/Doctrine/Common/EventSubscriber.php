da349ae4b73a1b67cd930974e54df1114e34620f
Fabien Potencier <fabien@potencier.org>
2022-05-11 14:54:47 +0200

    Fix typo (#45)

--- lib/Doctrine/Common/EventSubscriber.php
+++ lib/Doctrine/Common/EventSubscriber.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Doctrine\Common;
 
 /**
- * An EventSubscriber knows himself what events he is interested in.
+ * An EventSubscriber knows what events it is interested in.
  * If an EventSubscriber is added to an EventManager, the manager invokes
  * {@link getSubscribedEvents} and registers the subscriber as a listener for all
  * returned events.
